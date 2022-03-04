657a934e9cd237e75c655d4eea2afe4b3a06b10d
Siddhant Gupta <siddhangupta@deloitte.com>
2022-03-05 03:59:22 +0530

    Add sidebar component and course card style changes

---
+++ src/app/components/side-bar-cart/side-bar-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-cart',
+  templateUrl: './side-bar-cart.component.html',
+  styleUrls: ['./side-bar-cart.component.scss']
+})
+export class SideBarCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
